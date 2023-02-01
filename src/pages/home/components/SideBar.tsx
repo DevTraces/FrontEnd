2e39d9255716194c66e4658195ecb5c6bb14c8bf
codeisneverodd <codeisneverodd@gmail.com>
2023-02-02 00:35:50 +0900

    feat: create SideBar component

---
+++ src/pages/home/components/SideBar.tsx
@@ -0,0 +1,68 @@
+import { Flex, Icon, Text } from "@chakra-ui/react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { navs } from "../constants/nav";
+
+export default function SideBar() {
+  const router = useRouter();
+
+  return (
+    <Flex
+      direction="column"
+      bg="white"
+      display={{
+        sm: "none",
+        md: "block"
+      }}
+      w={{
+        md: 100,
+        xl: 250
+      }}
+      h="100vh"
+      pt={10}
+    >
+      <Flex as="nav" direction="column" gap="50px">
+        <Flex pl={10} gap={5}>
+          <Text color="red" fontSize="3xl" fontFamily="cursive">
+            A
+          </Text>
+          <Text
+            gap={10}
+            fontSize="3xl"
+            display={{ md: "none", xl: "inline-block" }}
+          >
+            Arterest
+          </Text>
+        </Flex>
+        {navs.map(({ key, title, icon, href }) => (
+          <Link href={href}>
+            <Flex
+              pl={10}
+              h={50}
+              gap={10}
+              align="center"
+              cursor="pointer"
+              fontWeight="bold"
+              _hover={{
+                bg: "red.400",
+                color: "white"
+              }}
+              color={router.pathname === href ? "red.400" : "black"}
+            >
+              <Icon key={key} as={FontAwesomeIcon} icon={icon} />
+              <Text
+                display={{
+                  sm: "none",
+                  xl: "block"
+                }}
+              >
+                {title}
+              </Text>
+            </Flex>
+          </Link>
+        ))}
+      </Flex>
+    </Flex>
+  );
+}
