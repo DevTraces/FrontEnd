fb5c8deabad5c457e554e330134738083fdba51a
codeisneverodd <codeisneverodd@gmail.com>
2023-02-02 15:04:59 +0900

    feat: Create SideBar component

--- src/pages/home/components/SideBar/index.tsx
+++ src/pages/home/components/SideBar/index.tsx
@@ -1,45 +1,69 @@
-import { Flex, Icon, Text } from "@chakra-ui/react";
+import {
+  Box,
+  Flex,
+  Icon,
+  Text,
+  useDisclosure,
+  useOutsideClick
+} from "@chakra-ui/react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import Link from "next/link";
-import { useRouter } from "next/router";
-import { NAVS } from "../../constants/nav";
+import { useRef, useState } from "react";
+import { Nav, NAVS } from "../../constants/nav";
+import Alert from "./Alert";
+import Like from "./Like";
+import Search from "./Search";
 
 export default function SideBar() {
-  const router = useRouter();
+  const { isOpen, onToggle, onClose, onOpen } = useDisclosure();
+  const [selectedNav, setSelectedNav] = useState<Nav["key"]>("home");
+  const drawerWantedNavs: Nav["key"][] = ["search", "alert", "like"];
+
+  const drawerRef = useRef(null);
+  useOutsideClick({
+    ref: drawerRef,
+    handler: e => {
+      const target = e.target as Element;
+      if (target.closest('[data-type="navItem"]')) return;
+      onClose();
+    }
+  });
 
   return (
-    <Flex
-      direction="column"
-      bg="white"
-      position="fixed"
-      top={0}
-      left={0}
-      display={{
-        sm: "none",
-        md: "block"
-      }}
-      w={{
-        md: 100,
-        xl: 250
-      }}
-      h="100vh"
-      pt={10}
-    >
-      <Flex as="nav" direction="column" gap="50px">
-        <Flex pl={10} gap={5}>
-          <Text color="red" fontSize="3xl" fontFamily="cursive">
-            A
-          </Text>
-          <Text
-            gap={10}
-            fontSize="3xl"
-            display={{ md: "none", xl: "inline-block" }}
-          >
-            Arterest
-          </Text>
-        </Flex>
-        {NAVS.map(({ key, title, icon, href }) => (
-          <Link href={href}>
+    <>
+      <Flex
+        direction="column"
+        bg="white"
+        position="fixed"
+        top={0}
+        left={0}
+        display={{
+          sm: "none",
+          md: "block"
+        }}
+        w={{
+          md: 100,
+          xl: 250
+        }}
+        h="100vh"
+        zIndex="modal"
+        pt={10}
+      >
+        <Flex as="nav" direction="column" gap="50px" data-type="navItem">
+          <Flex pl={10} gap={5}>
+            <Text color="red" fontSize="3xl" fontFamily="cursive">
+              A
+            </Text>
+            {!isOpen && (
+              <Text
+                gap={10}
+                fontSize="3xl"
+                display={{ md: "none", xl: "inline-block" }}
+              >
+                Arterest
+              </Text>
+            )}
+          </Flex>
+          {NAVS.map(({ key, title, icon }) => (
             <Flex
               pl={10}
               h={50}
@@ -51,21 +75,54 @@ export default function SideBar() {
                 bg: "red.400",
                 color: "white"
               }}
-              color={router.pathname === href ? "red.400" : "black"}
+              color={selectedNav === key ? "red.400" : "black"}
+              onClick={() => {
+                if (drawerWantedNavs.includes(key)) {
+                  if (key === selectedNav) {
+                    onToggle();
+                  } else {
+                    onOpen();
+                  }
+                } else {
+                  onClose();
+                }
+
+                setSelectedNav(key);
+              }}
             >
-              <Icon key={key} as={FontAwesomeIcon} icon={icon} />
-              <Text
-                display={{
-                  sm: "none",
-                  xl: "block"
-                }}
-              >
-                {title}
-              </Text>
+              <Icon key={key} as={FontAwesomeIcon} icon={icon} boxSize="20px" />
+              {!isOpen && (
+                <Text
+                  display={{
+                    sm: "none",
+                    xl: "block"
+                  }}
+                >
+                  {title}
+                </Text>
+              )}
             </Flex>
-          </Link>
-        ))}
+          ))}
+        </Flex>
       </Flex>
-    </Flex>
+      {drawerWantedNavs.includes(selectedNav) && (
+        <Box
+          zIndex="modal"
+          display={isOpen ? "block" : "none"}
+          position="fixed"
+          left="100px"
+          top={0}
+          p="40px"
+          w={300}
+          bg="white"
+          h="100vh"
+          ref={drawerRef}
+        >
+          {selectedNav === "search" && <Search />}
+          {selectedNav === "alert" && <Alert />}
+          {selectedNav === "like" && <Like />}
+        </Box>
+      )}
+    </>
   );
 }
