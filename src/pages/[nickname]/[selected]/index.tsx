763f7b764e410fb140277397de007eb0f4dd56bf
jiseung-kang <ill0o0lli@ajou.ac.kr>
2023-02-05 19:53:55 +0900

    feat: apply tabpanel using PostList&UserList

--- src/pages/[nickname]/[selected]/index.tsx
+++ src/pages/[nickname]/[selected]/index.tsx
@@ -1,19 +1,9 @@
 import NavLayout from "@/components/NavLayout";
-import {
-  Avatar,
-  Box,
-  Button,
-  Flex,
-  Tab,
-  TabList,
-  TabPanel,
-  TabPanels,
-  Tabs,
-  Text,
-  VStack
-} from "@chakra-ui/react";
+import PostList from "@/components/[nickname]/PostList";
+import ProfileInfo from "@/components/[nickname]/ProfileInfo";
+import UserList from "@/components/[nickname]/UserList";
+import { Tab, TabList, TabPanel, TabPanels, Tabs } from "@chakra-ui/react";
 import Head from "next/head";
-import Link from "next/link";
 import { useRouter } from "next/router";
 
 type Selected = "posts" | "saved" | "following" | "follower";
@@ -30,57 +20,14 @@ export default function Profile() {
         <title>Arterest | Profile</title>
       </Head>
       <NavLayout>
-        <Flex direction="column" pt="100px" w="max-content" pb="40px" m="auto">
-          <Flex
-            alignItems="center"
-            justifyContent="space-between"
-            mb="20px"
-            gap="20px"
-          >
-            <Avatar boxSize="80px" />
-            <Flex direction="row" gap="20px">
-              <VStack>
-                <Text fontWeight="bold">0</Text>
-                <Text>게시물</Text>
-              </VStack>
-              <VStack>
-                <Text fontWeight="bold">0</Text>
-                <Text>팔로잉</Text>
-              </VStack>
-              <VStack>
-                <Text fontWeight="bold">0</Text>
-                <Text>팔로워</Text>
-              </VStack>
-            </Flex>
-          </Flex>
-          <Flex
-            pb="20px"
-            gap="40px"
-            alignItems="center"
-            justifyContent="space-between"
-          >
-            <Box>
-              <Text fontWeight="bold" w="full" fontSize="xl">
-                {username}
-              </Text>
-              <Text fontSize="md" pr="8px" color="gray.700" w="full">
-                @{nickname}
-              </Text>
-            </Box>
-            <Box ml="40px">
-              <Link href="/accounts/edit">
-                <Button
-                  bg="gray.200"
-                  colorScheme="gray"
-                  fontWeight="bold"
-                  size="sm"
-                >
-                  프로필 편집
-                </Button>
-              </Link>
-            </Box>
-          </Flex>
-        </Flex>
+        <ProfileInfo
+          nickname={nickname as string}
+          username={username}
+          postCount={0}
+          followerCount={0}
+          followingCount={0}
+          imgURL=""
+        />
         {selected && (
           <Tabs
             variant="line"
@@ -119,10 +66,18 @@ export default function Profile() {
               </Tab>
             </TabList>
             <TabPanels>
-              <TabPanel>게시물</TabPanel>
-              <TabPanel>저장한 목록</TabPanel>
-              <TabPanel>팔로잉</TabPanel>
-              <TabPanel>팔로워</TabPanel>
+              <TabPanel>
+                <PostList />
+              </TabPanel>
+              <TabPanel>
+                <PostList />
+              </TabPanel>
+              <TabPanel>
+                <UserList />
+              </TabPanel>
+              <TabPanel>
+                <UserList />
+              </TabPanel>
             </TabPanels>
           </Tabs>
         )}
