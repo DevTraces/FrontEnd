8126c830d36a0436034f596395204818e255429d
codeisneverodd <codeisneverodd@gmail.com>
2023-02-08 00:20:12 +0900

    feat: add emailAuth validation and request

--- src/pages/auth/signUp/emailAuth/index.tsx
+++ src/pages/auth/signUp/emailAuth/index.tsx
@@ -1,3 +1,4 @@
+import { signUpUserAtom } from "@/atoms/auth/signUpUser";
 import AuthButton from "@/components/auth/AuthButton";
 import AuthLayout from "@/components/auth/AuthLayout";
 import AuthTextInput from "@/components/auth/AuthTextInput";
@@ -13,13 +14,54 @@ import {
 } from "@chakra-ui/react";
 import { faPaperPlane } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useMutation } from "@tanstack/react-query";
 import Head from "next/head";
 import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
+import { useRecoilValue } from "recoil";
+
+interface FormData {
+  authKey: string;
+}
 
 export default function EmailAuth() {
+  const [isClient, setIsClient] = useState(false);
   const router = useRouter();
   const toast = useToast();
-  const isError = false;
+  const user = useRecoilValue(signUpUserAtom);
+
+  const {
+    handleSubmit,
+    register,
+    setError,
+    formState: { errors, isSubmitting }
+  } = useForm<FormData>();
+
+  const emailMutation = useMutation({
+    mutationFn: ({ authKey }: FormData) => {
+      return fetch("/api/auth/email/auth-key/check", {
+        method: "POST",
+        body: JSON.stringify({
+          email: user.email,
+          authKey
+        })
+      });
+    },
+    onSuccess: async res => {
+      if (res.status !== 200 && !errors?.authKey) {
+        setError("authKey", { message: (await res.json()).errorMessage });
+      } else {
+        router.push("/auth/profile");
+      }
+    }
+  });
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
+  if (!isClient) return null;
 
   return (
     <>
@@ -36,8 +78,9 @@ export default function EmailAuth() {
           />
         </Center>
         <Text textAlign="center" wordBreak="keep-all">
-          codeisneverodd@gmail.com 주소로 전송된 인증코드를 입력하세요.
+          {user.email} 주소로 전송된 인증코드를 입력하세요.
           <Text
+            as="span"
             display="inline"
             color="primary"
             ml="10px"
@@ -54,22 +97,34 @@ export default function EmailAuth() {
             코드 재전송
           </Text>
         </Text>
-
-        <FormControl as={VStack} isInvalid={isError}>
-          <AuthTextInput type="number" placeholder="인증 코드" />
-          {!isError ? (
-            <FormHelperText />
-          ) : (
-            <FormErrorMessage>인증코드를 입력해주세요.</FormErrorMessage>
-          )}
-          <AuthButton
-            onClick={() => {
-              router.push("/auth/profile");
-            }}
-          >
-            다음
-          </AuthButton>
-        </FormControl>
+        <form
+          onSubmit={handleSubmit(value => {
+            emailMutation.mutate({ authKey: value.authKey });
+          })}
+          style={{ width: "100%" }}
+        >
+          <FormControl as={VStack} isInvalid={!!errors.authKey}>
+            <AuthTextInput
+              type="text"
+              placeholder="인증 코드"
+              {...register("authKey", {
+                required: "인증코드를 입력해야해요."
+              })}
+            />
+            {!errors.authKey ? (
+              <FormHelperText opacity={0} h="20px">
+                {user.email} 주소로 전송된 인증코드를 확인해주세요.
+              </FormHelperText>
+            ) : (
+              <FormErrorMessage h="20px" w="full">
+                {errors.authKey?.message}
+              </FormErrorMessage>
+            )}
+            <AuthButton type="submit" isLoading={isSubmitting}>
+              다음
+            </AuthButton>
+          </FormControl>
+        </form>
       </AuthLayout>
     </>
   );
