7e3543aa993d2348249da4bc9efd49448cd1ea2f
jiseung-kang <ill0o0lli@ajou.ac.kr>
2023-02-09 12:25:08 +0900

    feat: add bookmark api

--- src/pages/api/bookmark/[feedId].ts
+++ src/pages/api/bookmark/[feedId].ts
@@ -2,10 +2,10 @@ import type { NextApiRequest, NextApiResponse } from "next";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === "POST") {
-    res.status(200).json({ message: "북마크 요청" });
+    res.status(200).json(null);
   }
 
   if (req.method === "DELETE") {
-    res.status(200).json({ message: "북마크 취소" });
+    res.status(200).json(null);
   }
 }
--- src/pages/api/bookmark/index.ts
+++ src/pages/api/bookmark/index.ts
@@ -2,7 +2,7 @@ import type { NextApiRequest, NextApiResponse } from "next";
 
 type Data = {
   feedId: string;
-  imageLink: string;
+  imageUrl: string;
 };
 
 export default function handler(
@@ -13,22 +13,22 @@ export default function handler(
     res.status(200).json([
       {
         feedId: "1",
-        imageLink:
+        imageUrl:
           "https://images.unsplash.com/photo-1606312619070-d48b4c652a52?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
       },
       {
         feedId: "2",
-        imageLink:
+        imageUrl:
           "https://images.unsplash.com/photo-1614088685112-0a760b71a3c8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
       },
       {
         feedId: "3",
-        imageLink:
+        imageUrl:
           "https://images.unsplash.com/photo-1627647227768-705244233b56?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
       },
       {
         feedId: "4",
-        imageLink:
+        imageUrl:
           "https://images.unsplash.com/photo-1553452118-621e1f860f43?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
       }
     ]);
