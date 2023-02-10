f529ac334c519e06d45ab7170a1833f40db30df9
jiseung-kang <ill0o0lli@ajou.ac.kr>
2023-02-10 21:05:12 +0900

    feat: add api requests

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,22 +1,36 @@
-/* eslint-disable import/prefer-default-export */
-export async function get<T = any>({
-  path,
-  query
-}: {
+type Params = {
   path: string;
   query?: { [key in string]: unknown };
-}): Promise<T> {
-  const url = (() => {
-    if (!query) return path;
+};
 
-    const queryString = Object.entries(query)
-      .map(([key, value]) => `${key}=${value}`)
-      .join("&");
-    return `${path}?${queryString}`;
-  })();
+type HttpRequest = <T = any>(params: Params) => Promise<T>;
 
-  const res = await fetch(url);
+type GET = HttpRequest;
 
+type DELETE = HttpRequest;
+
+type PATCH = POST;
+
+type POST = <T = any>(
+  params: Params & {
+    body?: { [key in string]: unknown };
+  }
+) => Promise<T>;
+
+const defaultHeaders = {
+  "Content-Type": "application/json"
+};
+
+const getURL = (path: string, query?: { [key in string]: unknown }) => {
+  if (!query) return path;
+
+  const queryString = Object.entries(query)
+    .map(([key, value]) => `${key}=${value}`)
+    .join("&");
+  return `${path}?${queryString}`;
+};
+
+const handleResponse = async (res: Response) => {
   if (!res.ok) {
     const { errorMessage } = await res.json();
 
@@ -30,4 +44,32 @@ export async function get<T = any>({
   if (!data) throw new Error("응답에 data 속성이 없습니다.");
 
   return data;
-}
+};
+
+const get: GET = async ({ path, query }) => {
+  const url = getURL(path, query);
+  const res = await fetch(url);
+
+  return handleResponse(res);
+};
+
+const post: POST = async ({ path, query, body = {} }) => {
+  const url = getURL(path, query);
+
+  const res = await fetch(url, {
+    method: "POST",
+    headers: defaultHeaders,
+    body: JSON.stringify(body)
+  });
+
+  return handleResponse(res);
+};
+
+const api: { get: GET; post: POST; delete: DELETE; patch: PATCH } = {
+  get,
+  post,
+  delete: get,
+  patch: post
+};
+
+export default api;
