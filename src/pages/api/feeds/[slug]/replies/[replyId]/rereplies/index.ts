e21be65fdfd98d56aae48ecd52ba601b24470a0f
codeisneverodd <codeisneverodd@gmail.com>
2023-02-11 14:25:33 +0900

    feat: add data to api/feeds/[slug]/.../rereplies

--- src/pages/api/feeds/[slug]/replies/[replyId]/rereplies/index.ts
+++ src/pages/api/feeds/[slug]/replies/[replyId]/rereplies/index.ts
@@ -13,53 +13,57 @@ type NestedCommentData = {
 
 export default function handler(
   req: NextApiRequest,
-  res: NextApiResponse<NestedCommentData | NestedCommentData[]>
+  res: NextApiResponse<{ data: NestedCommentData | NestedCommentData[] }>
 ) {
   if (req.method === "POST") {
     res.status(200).json({
-      rereplyId: 1,
-      replyId: 1,
-      feedId: 1,
-      authorNickname: "1",
-      content: "대댓글 내용",
-      authorProfileImageUrl: "https://placehold.it/100x100",
-      createdAt: new Date(),
-      modifiedAt: new Date()
-    });
-  }
-
-  if (req.method === "GET") {
-    res.status(200).json([
-      {
+      data: {
         rereplyId: 1,
         replyId: 1,
         feedId: 1,
-        authorNickname: "3",
-        content: "대댓글 내용",
-        authorProfileImageUrl: "https://placehold.it/100x100",
-        createdAt: new Date(),
-        modifiedAt: new Date()
-      },
-      {
-        rereplyId: 2,
-        replyId: 1,
-        feedId: 1,
-        authorNickname: "4",
-        content: "대댓글 내용",
-        authorProfileImageUrl: "https://placehold.it/100x100",
-        createdAt: new Date(),
-        modifiedAt: new Date()
-      },
-      {
-        rereplyId: 3,
-        replyId: 1,
-        feedId: 1,
-        authorNickname: "7",
+        authorNickname: "1",
         content: "대댓글 내용",
         authorProfileImageUrl: "https://placehold.it/100x100",
         createdAt: new Date(),
         modifiedAt: new Date()
       }
-    ]);
+    });
+  }
+
+  if (req.method === "GET") {
+    res.status(200).json({
+      data: [
+        {
+          rereplyId: 1,
+          replyId: 1,
+          feedId: 1,
+          authorNickname: "3",
+          content: "대댓글 내용",
+          authorProfileImageUrl: "https://placehold.it/100x100",
+          createdAt: new Date(),
+          modifiedAt: new Date()
+        },
+        {
+          rereplyId: 2,
+          replyId: 1,
+          feedId: 1,
+          authorNickname: "4",
+          content: "대댓글 내용",
+          authorProfileImageUrl: "https://placehold.it/100x100",
+          createdAt: new Date(),
+          modifiedAt: new Date()
+        },
+        {
+          rereplyId: 3,
+          replyId: 1,
+          feedId: 1,
+          authorNickname: "7",
+          content: "대댓글 내용",
+          authorProfileImageUrl: "https://placehold.it/100x100",
+          createdAt: new Date(),
+          modifiedAt: new Date()
+        }
+      ]
+    });
   }
 }
