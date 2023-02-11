8cd0b3e6527b51b663d2568ff282c242ffcdd10f
codeisneverodd <codeisneverodd@gmail.com>
2023-02-11 14:23:46 +0900

    feat: add data to api/bookmark/[feedId]

--- src/pages/api/bookmark/[feedId].ts
+++ src/pages/api/bookmark/[feedId].ts
@@ -2,10 +2,10 @@ import type { NextApiRequest, NextApiResponse } from "next";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === "POST") {
-    res.status(200).json(null);
+    res.status(200).json({ data: null });
   }
 
   if (req.method === "DELETE") {
-    res.status(200).json(null);
+    res.status(200).json({ data: null });
   }
 }
