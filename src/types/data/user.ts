97338db2e9371f02be457adb816dd631800ca8e7
jiseung-kang <ill0o0lli@ajou.ac.kr>
2023-02-11 23:05:13 +0900

    feat: add data types

---
+++ src/types/data/user.ts
@@ -0,0 +1,11 @@
+export type ProfileTabName = "posts" | "saved" | "following" | "follower";
+
+export type ProfileData = {
+  username: string;
+  nickname: string;
+  description: string;
+  profileImageUrl: string;
+  totalFeedNumber: number;
+  followerNumber: number;
+  followingNumber: number;
+};
