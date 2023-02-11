e8f2f000fec5fa4c3ad4d120254bd2d7bfdb8284
codeisneverodd <codeisneverodd@gmail.com>
2023-02-11 14:21:33 +0900

    refactor: detach follower, following list api

--- src/components/[nickname]/ProfileFeed/FollowList.tsx
+++ src/components/[nickname]/ProfileFeed/FollowList.tsx
@@ -1,24 +1,32 @@
-import { getFollowList } from "@/api/follows/[selected]/[nickname]";
+import { getFollowerList } from "@/api/follows/follower/[nickname]";
+import { getFollowingList } from "@/api/follows/following/[nickname]";
 import { VStack } from "@chakra-ui/react";
 import { useQuery } from "@tanstack/react-query";
 import FollowItem from "./FollowItem";
 
 type FollowListProps = {
   nickname: string;
-  selected: string;
+  type: "follower" | "following";
 };
 
-export default function FollowList({ nickname, selected }: FollowListProps) {
+export default function FollowList({ nickname, type }: FollowListProps) {
   const followListQuery = useQuery({
-    queryKey: ["followList", nickname, selected],
-    queryFn: ({ queryKey }) => getFollowList(queryKey[1], queryKey[2])
+    queryKey: ["followList", nickname, type],
+    queryFn: ({ queryKey }) => {
+      // TODO: pagination 필요
+      if (queryKey[2] === "follower") {
+        return getFollowerList(queryKey[2], 1, 1);
+      }
+      return getFollowingList(queryKey[2], 1, 1);
+    }
   });
+
   if (followListQuery.isError) return <>FollowList 에러 발생</>;
   if (followListQuery.isLoading) return <>FollowList 로딩 중...</>;
 
   return (
     <VStack>
-      {followListQuery.data?.map(d => (
+      {followListQuery.data.map(d => (
         <FollowItem key={d.nickname} {...d} />
       ))}
     </VStack>
