f7fd54b215537636f94b8799cfcddc8c2030b8e2
codeisneverodd <codeisneverodd@gmail.com>
2023-02-11 16:03:56 +0900

    feat: add response type for bookmark api

--- src/api/bookmark/[feedId].ts
+++ src/api/bookmark/[feedId].ts
@@ -1,9 +1,9 @@
 import api from "@/api";
 
 export const postBoomark = (feedId: number) => {
-  return api.post({ path: `/api/bookmark/${feedId}` });
+  return api.post<null>({ path: `/api/bookmark/${feedId}` });
 };
 
 export const deleteBookmark = (feedId: number) => {
-  return api.delete({ path: `/api/bookmark/${feedId}` });
+  return api.delete<null>({ path: `/api/bookmark/${feedId}` });
 };
