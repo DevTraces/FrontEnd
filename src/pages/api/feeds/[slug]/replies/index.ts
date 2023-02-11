d322bbcaee97bfcc089a4edd0462edef3b76c928
codeisneverodd <codeisneverodd@gmail.com>
2023-02-11 14:24:43 +0900

    feat: add data to api/feeds/[slug]/replies

--- src/pages/api/feeds/[slug]/replies/index.ts
+++ src/pages/api/feeds/[slug]/replies/index.ts
@@ -13,24 +13,11 @@ type CommentData = {
 
 export default function handler(
   req: NextApiRequest,
-  res: NextApiResponse<CommentData | CommentData[]>
+  res: NextApiResponse<{ data: CommentData | CommentData[] }>
 ) {
   if (req.method === "POST") {
     res.status(200).json({
-      replyId: 1,
-      feedId: 1,
-      authorNickname: "1",
-      content: "댓글 내용",
-      authorProfileImageUrl: "https://placehold.it/100x100",
-      numberOfRereply: 8,
-      createdAt: new Date(),
-      modifiedAt: new Date()
-    });
-  }
-
-  if (req.method === "GET") {
-    res.status(200).json([
-      {
+      data: {
         replyId: 1,
         feedId: 1,
         authorNickname: "1",
@@ -39,17 +26,34 @@ export default function handler(
         numberOfRereply: 8,
         createdAt: new Date(),
         modifiedAt: new Date()
-      },
-      {
-        replyId: 2,
-        feedId: 1,
-        authorNickname: "2",
-        content: "댓글 내용",
-        authorProfileImageUrl: "https://placehold.it/100x100",
-        numberOfRereply: 8,
-        createdAt: new Date(),
-        modifiedAt: new Date()
       }
-    ]);
+    });
+  }
+
+  if (req.method === "GET") {
+    res.status(200).json({
+      data: [
+        {
+          replyId: 1,
+          feedId: 1,
+          authorNickname: "1",
+          content: "댓글 내용",
+          authorProfileImageUrl: "https://placehold.it/100x100",
+          numberOfRereply: 8,
+          createdAt: new Date(),
+          modifiedAt: new Date()
+        },
+        {
+          replyId: 2,
+          feedId: 1,
+          authorNickname: "2",
+          content: "댓글 내용",
+          authorProfileImageUrl: "https://placehold.it/100x100",
+          numberOfRereply: 8,
+          createdAt: new Date(),
+          modifiedAt: new Date()
+        }
+      ]
+    });
   }
 }
