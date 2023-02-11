15cc636b533f8f9f52c4569dc93e401d203e42a4
codeisneverodd <codeisneverodd@gmail.com>
2023-02-11 14:25:00 +0900

    feat: add data to api/feeds/[slug]/replies/[replyId]

--- src/pages/api/feeds/[slug]/replies/[replyId]/index.ts
+++ src/pages/api/feeds/[slug]/replies/[replyId]/index.ts
@@ -12,21 +12,23 @@ type CommentData = {
 
 export default function handler(
   req: NextApiRequest,
-  res: NextApiResponse<CommentData | null>
+  res: NextApiResponse<{ data: CommentData | null }>
 ) {
   if (req.method === "PUT") {
     res.status(200).json({
-      replyId: 1,
-      feedId: 1,
-      authorNickname: "1",
-      content: "댓글 내용",
-      authorProfileImageUrl: "https://placehold.it/100x100",
-      createdAt: new Date(),
-      modifiedAt: new Date()
+      data: {
+        replyId: 1,
+        feedId: 1,
+        authorNickname: "1",
+        content: "댓글 내용",
+        authorProfileImageUrl: "https://placehold.it/100x100",
+        createdAt: new Date(),
+        modifiedAt: new Date()
+      }
     });
   }
 
   if (req.method === "DELETE") {
-    res.status(200).json(null);
+    res.status(200).json({ data: null });
   }
 }
