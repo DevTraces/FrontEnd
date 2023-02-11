f8aef44de966b5d0e02cda58b68f3e155320dd88
codeisneverodd <codeisneverodd@gmail.com>
2023-02-11 16:04:45 +0900

    refactor: detach response typeof getUserProfile

--- src/api/users/profile/[nickname].ts
+++ src/api/users/profile/[nickname].ts
@@ -1,13 +1,14 @@
 import api from "@/api";
 
+type UserProfileData = {
+  username: string;
+  nickname: string;
+  description: string;
+  profileImageUrl: string;
+  totalFeedNumber: number;
+  followerNumber: number;
+  followingNumber: number;
+};
 export async function getUserProfile(nickname: string) {
-  return api.get<{
-    username: string;
-    nickname: string;
-    description: string;
-    profileImageUrl: string;
-    totalFeedNumber: number;
-    followerNumber: number;
-    followingNumber: number;
-  }>({ path: `/api/users/profile/${nickname}` });
+  return api.get<UserProfileData>({ path: `/api/users/profile/${nickname}` });
 }
