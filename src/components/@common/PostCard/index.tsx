e08f8e7ffc01e4117d438194be07f784e828a08a
codeisneverodd <codeisneverodd@gmail.com>
2023-02-14 16:06:10 +0900

    feat: detach logics to component

--- src/components/@common/PostCard/index.tsx
+++ src/components/@common/PostCard/index.tsx
@@ -1,24 +1,10 @@
 import { PostData } from "@/types/data/post";
-import {
-  Avatar,
-  Card,
-  CardBody,
-  CardFooter,
-  CardHeader,
-  Flex,
-  Icon,
-  Input,
-  InputGroup,
-  InputLeftElement,
-  InputRightElement,
-  Text
-} from "@chakra-ui/react";
-import { faPaperPlane, faSmile } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { ComponentProps } from "react";
+import { Avatar, Card, Flex, Text } from "@chakra-ui/react";
+import { ComponentProps, useState } from "react";
 import Carousel from "./components/Carousel";
-import Content from "./components/Content";
-import ReplyList from "./components/Reply";
+import ReplyList from "./components/ReplyList";
+import TextContent from "./components/TextContent";
+import Toolbar from "./components/Toolbar";
 
 type PostCardProps = PostData & ComponentProps<typeof Card>;
 
@@ -34,45 +20,36 @@ export default function PostCard({
   saved,
   ...restProps
 }: PostCardProps) {
+  const [isReplyOpen, setIsReplyOpen] = useState(false);
+
   return (
-    <Card w="md" zIndex="base" {...restProps}>
-      <CardHeader px={0}>
-        <Flex alignItems="center" gap={4} px={4}>
-          <Avatar boxSize={10} />
-          <Text fontWeight="bold">{authorNickname}</Text>
-        </Flex>
-      </CardHeader>
-      <Carousel imgs={imageUrls} />
-      <CardBody px="12px">
-        <Content
-          feedId={feedId}
-          authorNickname={authorNickname}
-          numberOfLike={numberOfLike}
-          hashtags={hashtags}
-          content={content}
-          createdAt={createdAt}
-          liked={liked}
-          saved={saved}
-        />
-        <ReplyList feedId={feedId} />
-      </CardBody>
-      <CardFooter p="0">
-        <InputGroup>
-          <InputLeftElement>
-            <Icon as={FontAwesomeIcon} icon={faSmile} color="gray.300" />
-          </InputLeftElement>
-          <Input
-            variant="filled"
-            focusBorderColor="white"
-            bg="white"
-            placeholder="댓글 달기..."
-            _hover={{ bg: "none" }}
-          />
-          <InputRightElement>
-            <Icon as={FontAwesomeIcon} icon={faPaperPlane} color="gray.300" />
-          </InputRightElement>
-        </InputGroup>
-      </CardFooter>
-    </Card>
+    <Flex
+      direction="column"
+      bg="white"
+      rounded="12px"
+      w="450px"
+      zIndex="base"
+      {...restProps}
+    >
+      <Flex alignItems="center" gap={4} px="12px" py="20px">
+        <Avatar boxSize={10} />
+        <Text fontWeight="bold">{authorNickname}</Text>
+      </Flex>
+      <Carousel imgs={imageUrls} boxSize={450} />
+      <TextContent
+        content={content}
+        authorNickname={authorNickname}
+        numberOfLike={numberOfLike}
+        hashtags={hashtags}
+        createdAt={createdAt}
+      />
+      <Toolbar
+        feedId={feedId}
+        liked={liked}
+        saved={saved}
+        setIsReplyOpen={setIsReplyOpen}
+      />
+      {isReplyOpen && <ReplyList feedId={feedId} />}
+    </Flex>
   );
 }
