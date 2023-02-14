7abec3a43231bae23f128e26501c8e08c37cf7a0
jiseung-kang <ill0o0lli@ajou.ac.kr>
2023-02-14 15:45:46 +0900

    fix: remove ununused import

--- src/pages/post/[pid]/edit.tsx
+++ src/pages/post/[pid]/edit.tsx
@@ -1,18 +1,13 @@
 import NavLayout from "@/components/@common/NavLayout";
-import PostForm from "@/components/@common/PostForm";
 
 import Head from "next/head";
-import { useRouter } from "next/router";
 
 export default function PostEdit() {
-  const router = useRouter();
-
   return (
     <NavLayout>
       <Head>
         <title>ArtBubble | Upload</title>
       </Head>
-      <PostForm feedId={+(router.query.pid as string)} />
     </NavLayout>
   );
 }
