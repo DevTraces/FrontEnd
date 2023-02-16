33ec70ceea538299770a0162a2e410ba2f04a640
codeisneverodd <codeisneverodd@gmail.com>
2023-02-17 00:04:50 +0900

    feat: add putFeed api

--- src/api/feeds/[feedId]/index.ts
+++ src/api/feeds/[feedId]/index.ts
@@ -1,5 +1,5 @@
 import api from "@/api";
-import { FeedData } from "@/types/data/feed";
+import { EditorImage, FeedData } from "@/types/data/feed";
 
 export const getFeed = async (feedId: number) => {
   return api.get<FeedData>({
@@ -9,3 +9,16 @@ export const getFeed = async (feedId: number) => {
 
 export const deleteFeed = (feedId: number) =>
   api.delete({ path: `/api/feeds/${feedId}` });
+
+export const putFeed = (
+  feedId: number,
+  modifiedData: {
+    content?: string;
+    images?: EditorImage[];
+    hashtags?: string[];
+  }
+) =>
+  api.put({
+    path: `/api/feeds/${feedId}`,
+    body: modifiedData
+  });
