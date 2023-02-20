402a4ec92fd54916c1664a1ffc8a925800a68d0a
codeisneverodd <codeisneverodd@gmail.com>
2023-02-20 18:17:54 +0900

    feat: change fetch api to axios

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,118 +1,35 @@
-const END_POINT = "http://54.180.200.170:8080";
+import axios, { AxiosInstance } from "axios";
 
-type Params = {
-  path: string;
-  query?: { [key in string | number]: string | number };
-  mode?: "prod";
-};
+interface CustomInstance extends AxiosInstance {
+  get<T>(...params: Parameters<AxiosInstance["get"]>): Promise<T>;
+  delete<T>(...params: Parameters<AxiosInstance["delete"]>): Promise<T>;
+  post<T>(...params: Parameters<AxiosInstance["post"]>): Promise<T>;
+  put<T>(...params: Parameters<AxiosInstance["put"]>): Promise<T>;
+  patch<T>(...params: Parameters<AxiosInstance["patch"]>): Promise<T>;
+}
 
-type HttpRequest = <T = any>(params: Params) => Promise<T>;
-
-type GET = HttpRequest;
-
-type DELETE = HttpRequest;
-
-type PATCH = POST;
-
-type PUT = POST;
-
-type POST = <T = any>(
-  params: Params & {
-    body?: { [key in PropertyKey]: any };
-  }
-) => Promise<T>;
-
-const defaultHeaders = {
-  "Content-Type": "application/json"
-};
-
-const getURL = (
-  path: Params["path"],
-  query?: Params["query"],
-  mode?: Params["mode"]
-) => {
-  const queryString = query
-    ? `?${Object.entries(query)
-        .map(([key, value]) => `${key}=${value}`)
-        .join("&")}`
-    : "";
-  const endpoint = mode === "prod" ? END_POINT : "";
-
-  return `${endpoint}${path}${queryString}`;
-};
-
-const handleResponse = async (
-  url: ReturnType<typeof getURL>,
-  res: Response
-) => {
-  if (!res.ok) {
-    const { errorMessage } = await res.json();
-
-    if (!errorMessage)
-      throw new Error(`${url} 에러에 errorMessage가 없습니다.`);
-
-    throw new Error(errorMessage);
-  }
-
-  const data = await res.json();
-
-  if (!Object.hasOwn(data, "data"))
-    throw new Error(`${url} 응답에 data 속성이 없습니다.`);
-
-  return data.data;
-};
-
-const get: GET = async ({ path, query, mode }) => {
-  const url = getURL(path, query, mode);
-  const res = await fetch(url, {
-    headers: defaultHeaders
+const axiosInstance = (baseURL: string = ""): CustomInstance => {
+  const instance = axios.create({
+    baseURL,
+    headers: { "Content-Type": "application/json" },
+    transformResponse: res => {
+      const json = JSON.parse(res);
+      if (!Object.hasOwn(json, "data")) {
+        // eslint-disable-next-line no-console
+        console.error(res);
+        throw new Error("응답에 data 속성이 없습니다.");
+      }
+      return json.data;
+    }
   });
 
-  return handleResponse(url, res);
+  instance.interceptors.response.use(res => res.data);
+
+  return instance;
 };
 
-const post: POST = async ({ path, query, body = {}, mode }) => {
-  const url = getURL(path, query, mode);
-
-  const res = await fetch(url, {
-    method: "POST",
-    headers: defaultHeaders,
-    body: JSON.stringify(body)
-  });
-
-  return handleResponse(url, res);
+const api = {
+  dev: axiosInstance(),
+  prod: axiosInstance("http://54.180.200.170:8080")
 };
-
-const patch: PATCH = async ({ path, query, body = {}, mode }) => {
-  const url = getURL(path, query, mode);
-
-  const res = await fetch(url, {
-    method: "PATCH",
-    headers: defaultHeaders,
-    body: JSON.stringify(body)
-  });
-
-  return handleResponse(url, res);
-};
-
-const put: PUT = async ({ path, query, body = {}, mode }) => {
-  const url = getURL(path, query, mode);
-
-  const res = await fetch(url, {
-    method: "PUT",
-    headers: defaultHeaders,
-    body: JSON.stringify(body)
-  });
-
-  return handleResponse(url, res);
-};
-
-const api: { get: GET; post: POST; delete: DELETE; patch: PATCH; put: PUT } = {
-  get,
-  post,
-  delete: get,
-  patch,
-  put
-};
-
 export default api;
