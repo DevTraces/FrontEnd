650280c429200e197c39abedfa8de171ced574a2
codeisneverodd <codeisneverodd@gmail.com>
2023-02-22 00:20:24 +0900

    feat: add update to useRereeply hook

--- src/hooks/useRereply.ts
+++ src/hooks/useRereply.ts
@@ -1,18 +1,26 @@
 import { postRereplies } from "@/api/feeds/[feedId]/replies/[replyId]/rereplies";
-import { deleteRereply } from "@/api/feeds/[feedId]/replies/[replyId]/rereplies/[rereplyId]";
+import {
+  deleteRereply,
+  putRereply
+} from "@/api/feeds/[feedId]/replies/[replyId]/rereplies/[rereplyId]";
 import feedsKeys from "@/queryKeys/feedsKeys";
 import { useToast } from "@chakra-ui/react";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 type UseRereplyConfig = {
   onCreate?: () => void;
+  onUpdate?: () => void;
   onDelete?: () => void;
 };
 
 export default function useRereply(
   feedId: number,
   replyId: number,
-  { onCreate = () => {}, onDelete = () => {} }: UseRereplyConfig = {}
+  {
+    onCreate = () => {},
+    onUpdate = () => {},
+    onDelete = () => {}
+  }: UseRereplyConfig = {}
 ) {
   const queryClient = useQueryClient();
   const toast = useToast();
@@ -34,6 +42,30 @@ export default function useRereply(
       });
     }
   });
+
+  const updateMutation = useMutation({
+    mutationFn: ({
+      rereplyId,
+      content
+    }: {
+      rereplyId: number;
+      content: string;
+    }) => putRereply(feedId, replyId, rereplyId, content),
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: feedsKeys.rereplies(feedId, replyId)
+      });
+      onUpdate();
+    },
+    onError: () => {
+      toast({
+        title: "대댓글 수정에 실패했어요",
+        status: "error",
+        duration: 1000
+      });
+    }
+  });
+
   const deleteMutation = useMutation({
     mutationFn: ({ rereplyId }: { rereplyId: number }) =>
       deleteRereply(feedId, replyId, rereplyId),
@@ -53,6 +85,8 @@ export default function useRereply(
   });
   return {
     create: (content: string) => createMutation.mutate({ content }),
+    update: (rereplyId: number, content: string) =>
+      updateMutation.mutate({ rereplyId, content }),
     delete: (rereplyId: number) => deleteMutation.mutate({ rereplyId })
   };
 }
