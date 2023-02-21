ed8412d7f82a5c80f42e3e20073575528d59d6bd
codeisneverodd <codeisneverodd@gmail.com>
2023-02-22 00:20:09 +0900

    feat: add update to useReply hook

--- src/hooks/useReply.ts
+++ src/hooks/useReply.ts
@@ -1,17 +1,22 @@
 import { postReplies } from "@/api/feeds/[feedId]/replies";
-import { deleteReply } from "@/api/feeds/[feedId]/replies/[replyId]";
+import { deleteReply, putReply } from "@/api/feeds/[feedId]/replies/[replyId]";
 import feedsKeys from "@/queryKeys/feedsKeys";
 import { useToast } from "@chakra-ui/react";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 type UseReplyConfig = {
   onCreate?: () => void;
+  onUpdate?: () => void;
   onDelete?: () => void;
 };
 
 export default function useReply(
   feedId: number,
-  { onCreate = () => {}, onDelete = () => {} }: UseReplyConfig = {}
+  {
+    onCreate = () => {},
+    onUpdate = () => {},
+    onDelete = () => {}
+  }: UseReplyConfig = {}
 ) {
   const queryClient = useQueryClient();
   const toast = useToast();
@@ -26,6 +31,21 @@ export default function useReply(
     onError: () => {
       toast({
         title: "댓글 등록에 실패했어요",
+        status: "error",
+        duration: 1000
+      });
+    }
+  });
+  const updateMutation = useMutation({
+    mutationFn: ({ replyId, content }: { replyId: number; content: string }) =>
+      putReply(feedId, replyId, content),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: feedsKeys.replies(feedId) });
+      onUpdate();
+    },
+    onError: () => {
+      toast({
+        title: "댓글 수정에 실패했어요",
         status: "error",
         duration: 1000
       });
@@ -50,6 +70,8 @@ export default function useReply(
 
   return {
     create: (content: string) => createMutation.mutate({ content }),
+    update: (replyId: number, content: string) =>
+      updateMutation.mutate({ replyId, content }),
     delete: (replyId: number) => deleteMutation.mutate({ replyId })
   };
 }
