1b06ea978e332cbf6a5e2018440db1226ec977ff
codeisneverodd <codeisneverodd@gmail.com>
2023-02-22 00:18:22 +0900

    feat: add putRereply api

--- src/api/feeds/[feedId]/replies/[replyId]/rereplies/[rereplyId].ts
+++ src/api/feeds/[feedId]/replies/[replyId]/rereplies/[rereplyId].ts
@@ -8,3 +8,14 @@ export const deleteRereply = (
   api.prod.delete<null>(
     `/api/feeds/${feedId}/replies/${replyId}/rereplies/${rereplyId}`
   );
+
+export const putRereply = (
+  feedId: number,
+  replyId: number,
+  rereplyId: number,
+  content: string
+) =>
+  api.prod.put<null>(
+    `/api/feeds/${feedId}/replies/${replyId}/rereplies/${rereplyId}`,
+    { content }
+  );
