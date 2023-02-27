2b5cd43a2357c8bebc6c2ebe4a86dc775a6f59b8
codeisneverodd <codeisneverodd@gmail.com>
2023-02-27 18:21:14 +0900

    feat: add patchUserProfile api

--- src/api/users/profile/[nickname].ts
+++ src/api/users/profile/[nickname].ts
@@ -1,5 +1,13 @@
 import api from "@/api";
-import { ProfileData } from "@/types/data/user";
+import { ProfilePatchData, ProfileData } from "@/types/data/user";
 
 export const getUserProfile = (nickname: string) =>
   api.prod.get<ProfileData>(`/api/users/profile/${nickname}`);
+
+export const patchUserProfile = ({
+  nickname,
+  data
+}: {
+  nickname: string;
+  data: Partial<ProfilePatchData>;
+}) => api.prod.patch<ProfilePatchData>(`/api/users/profile/${nickname}`, data);
