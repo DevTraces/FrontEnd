c146852f1be2d88dbdf53b9b512be4eb66d1b328
codeisneverodd <codeisneverodd@gmail.com>
2023-02-28 23:04:27 +0900

    feat: add type SearchValue

--- src/types/data/search.ts
+++ src/types/data/search.ts
@@ -3,3 +3,9 @@ export type UserSearchResultData = {
   nickname: string;
   profileImageUrl: string;
 };
+
+export type SearchValue = {
+  type: "username" | "nickname" | "tag";
+  value: string;
+  isTyping: boolean;
+};
