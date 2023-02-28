5d65d85afc69ec2e09863efa64acd3a6d6091f23
codeisneverodd <codeisneverodd@gmail.com>
2023-02-28 22:59:00 +0900

    feat: add left marker for SearchInput

--- src/components/@common/NavLayout/components/Search/components/Input/index.tsx
+++ src/components/@common/NavLayout/components/Search/components/Input/index.tsx
@@ -1,42 +1,89 @@
+import useSearch from "@/hooks/useSearch";
 import {
   Icon,
   Input,
   InputGroup,
   InputLeftElement,
-  InputRightElement
+  InputRightElement,
+  Spinner
 } from "@chakra-ui/react";
-import { faSearch, faCircleXmark } from "@fortawesome/free-solid-svg-icons";
+import { faCircleXmark } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { useContext } from "react";
-import { SearchContext } from "../../../SearchContext";
+import {
+  ChangeEventHandler,
+  KeyboardEventHandler,
+  useEffect,
+  useState
+} from "react";
 
 type InputContainerProps = React.ComponentProps<typeof InputGroup>;
 
 export default function InputContainer({ ...restProps }: InputContainerProps) {
-  const { search, setSearch } = useContext(SearchContext);
+  const [value, setValue] = useState("");
+  const [leftMark, setLeftMark] = useState(" ");
+  const { searchValue, changeType, search } = useSearch();
 
-  const handleInputChange: React.ChangeEventHandler<HTMLInputElement> = e => {
-    setSearch(e.target.value);
+  useEffect(() => {
+    if (leftMark !== "@" && searchValue.type === "nickname") setLeftMark("@");
+    if (leftMark !== "#" && searchValue.type === "tag") setLeftMark("#");
+    if (leftMark !== " " && searchValue.type === "username") setLeftMark(" ");
+  }, [searchValue, setLeftMark, leftMark]);
+  const handleInputChange: ChangeEventHandler<HTMLInputElement> = e => {
+    const newValue = e.target.value;
+
+    if (newValue.startsWith("@")) {
+      setLeftMark("@");
+      changeType("nickname");
+      setValue(newValue.slice(1));
+      search(newValue.slice(1));
+    } else if (newValue.startsWith("#")) {
+      setLeftMark("#");
+      changeType("tag");
+      setValue(newValue.slice(1));
+      search(newValue.slice(1));
+    } else {
+      setValue(newValue);
+      search(newValue);
+    }
+  };
+
+  const handleInputKeyDown: KeyboardEventHandler<HTMLInputElement> = e => {
+    if (value === "" && (e.key === "Backspace" || e.key === "Delete")) {
+      changeType("username");
+      setLeftMark("");
+    }
   };
 
   const handleResetClick = () => {
-    setSearch("");
+    setValue("");
+    search("");
   };
 
   return (
     <InputGroup data-type="navItem" {...restProps}>
-      <InputLeftElement pointerEvents="none">
-        <Icon as={FontAwesomeIcon} icon={faSearch} color="gray.300" />
-      </InputLeftElement>
+      <InputLeftElement pointerEvents="none">{leftMark}</InputLeftElement>
       <Input
         type="text"
         bg="gray.200"
         placeholder="검색"
-        value={search}
+        value={value}
+        focusBorderColor="primary"
         onChange={handleInputChange}
+        onKeyDown={handleInputKeyDown}
       />
+
       <InputRightElement onClick={handleResetClick}>
-        <Icon as={FontAwesomeIcon} icon={faCircleXmark} color="gray.300" />
+        {searchValue.isTyping ? (
+          <Spinner
+            boxSize="16px"
+            thickness="2px"
+            speed="0.7s"
+            emptyColor="gray.200"
+            color="primary"
+          />
+        ) : (
+          <Icon as={FontAwesomeIcon} icon={faCircleXmark} color="gray.300" />
+        )}
       </InputRightElement>
     </InputGroup>
   );
