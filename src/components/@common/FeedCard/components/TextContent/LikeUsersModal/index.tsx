972bc48d542664b1e415048cceefba7669667327
codeisneverodd <codeisneverodd@gmail.com>
2023-02-28 14:10:45 +0900

    feat: create LikeUsersModal component

---
+++ src/components/@common/FeedCard/components/TextContent/LikeUsersModal/index.tsx
@@ -0,0 +1,89 @@
+import { getFeedsLikeUsers } from "@/api/feeds/like/[feedId]";
+import feedAtom from "@/atoms/feedAtom";
+import ProfileAvatar from "@/components/@common/ProfileAvatar";
+import feedsKeys from "@/queryKeys/feedsKeys";
+import {
+  Button,
+  Flex,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalHeader,
+  ModalOverlay,
+  Spinner,
+  Text
+} from "@chakra-ui/react";
+import { useQuery } from "@tanstack/react-query";
+import { useRouter } from "next/router";
+import { useRecoilValue } from "recoil";
+
+type LikeUsersModalProps = {
+  disclosure: {
+    isOpen: boolean;
+    onOpen: () => void;
+    onClose: () => void;
+    onToggle: () => void;
+    isControlled: boolean;
+    getButtonProps: (props?: any) => any;
+    getDisclosureProps: (props?: any) => any;
+  };
+};
+
+export default function LikeUsersModal({
+  disclosure: { isOpen, onClose }
+}: LikeUsersModalProps) {
+  const { feedId } = useRecoilValue(feedAtom);
+  const router = useRouter();
+  const { data } = useQuery({
+    queryKey: feedsKeys.likeUsers(feedId),
+    queryFn: () => getFeedsLikeUsers({ feedId, page: 0 })
+  });
+
+  return (
+    <Modal isOpen={isOpen} onClose={onClose} isCentered>
+      <ModalOverlay />
+      <ModalContent h="440px">
+        <ModalCloseButton top="16px" />
+        <ModalHeader textAlign="center" borderBottom="0.5px solid gray">
+          <Text>좋아요</Text>
+        </ModalHeader>
+        <ModalBody py="20px" px="24px">
+          {data ? (
+            <Flex direction="column" gap="24px">
+              {data.map(({ userName, nickname, profileImageLink }) => (
+                <Flex
+                  key={nickname}
+                  alignItems="center"
+                  gap="20px"
+                  w="full"
+                  cursor="pointer"
+                >
+                  <Flex
+                    alignItems="center"
+                    gap="20px"
+                    flex="1"
+                    onClick={() => {
+                      router.push(`/user/${nickname}/posts`);
+                    }}
+                  >
+                    <ProfileAvatar
+                      src={profileImageLink}
+                      size="44px"
+                      alt="프로필 이미지"
+                    />
+                    <Text fontWeight="bold">{userName}</Text>
+                  </Flex>
+
+                  <Button>팔로우</Button>
+                </Flex>
+              ))}
+            </Flex>
+          ) : (
+            <Spinner />
+          )}
+        </ModalBody>
+      </ModalContent>
+    </Modal>
+  );
+}
