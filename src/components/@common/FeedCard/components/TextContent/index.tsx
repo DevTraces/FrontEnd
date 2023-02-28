26e7d18c3f4fb44b9c1703cfe2da659bda54f715
codeisneverodd <codeisneverodd@gmail.com>
2023-02-28 14:10:22 +0900

    feat: add LikeUsersModal

--- src/components/@common/FeedCard/components/TextContent/index.tsx
+++ src/components/@common/FeedCard/components/TextContent/index.tsx
@@ -1,11 +1,12 @@
 import feedAtom from "@/atoms/feedAtom";
 import getDateFormat from "@/utils/date";
-import { Box, Button, Flex, HStack, Text } from "@chakra-ui/react";
+import { Button, Flex, HStack, Text, useDisclosure } from "@chakra-ui/react";
 import Link from "next/link";
 import { useEffect, useState } from "react";
 import { useRecoilValue } from "recoil";
+import LikeUsersModal from "./LikeUsersModal";
 
-const MAX_PREVIEW_LENGTH = 10;
+const MAX_PREVIEW_LENGTH = 40;
 
 export default function TextContent() {
   const { numberOfLike, authorNickname, hashtags, content, createdAt } =
@@ -14,42 +15,55 @@ export default function TextContent() {
   const contentPreview = content.slice(0, MAX_PREVIEW_LENGTH);
   const contentMore = content.slice(MAX_PREVIEW_LENGTH);
 
+  const likeUsersDisclosure = useDisclosure();
+
   useEffect(() => {
     if (content.length < MAX_PREVIEW_LENGTH) setIsMoreLoaded(true);
   }, [setIsMoreLoaded, content]);
 
   return (
-    <Box px="12px" py="20px">
-      <Text fontWeight="bold" textAlign="left">
-        좋아요 {numberOfLike}개
-      </Text>
-      <Flex direction="column" alignItems="flex-start" w="full">
-        <Text fontWeight="bold">{authorNickname}</Text>
-        <HStack>
-          {hashtags.map(tag => (
-            <Link href={`/explore/tags/${tag}`} key={tag}>
-              <Text color="blue.700">#{tag}</Text>
-            </Link>
-          ))}
-        </HStack>
-        <Text>{contentPreview}</Text>
-
-        {isMoreLoaded ? (
-          <Text>{contentMore}</Text>
-        ) : (
-          <Button
-            as={Button}
-            width="full"
-            color="blue.700"
-            onClick={() => setIsMoreLoaded(!isMoreLoaded)}
-          >
-            더보기
-          </Button>
-        )}
-        <Text fontSize="12px" color="gray">
-          {getDateFormat(createdAt)}
+    <>
+      {likeUsersDisclosure.isOpen && (
+        <LikeUsersModal disclosure={likeUsersDisclosure} />
+      )}
+      <Flex px="12px" py="20px" direction="column" gap="12px" w="full">
+        <Text
+          fontWeight="bold"
+          textAlign="left"
+          onClick={likeUsersDisclosure.onOpen}
+          cursor="pointer"
+        >
+          좋아요 {numberOfLike}개
         </Text>
+        <Flex direction="column" alignItems="flex-start" w="full" gap="8px">
+          <Text fontWeight="bold">{authorNickname}</Text>
+          <HStack>
+            {hashtags.map(tag => (
+              <Link href={`/explore/tags/${tag}`} key={tag}>
+                <Text color="blue.700">#{tag}</Text>
+              </Link>
+            ))}
+          </HStack>
+          <Text>
+            {contentPreview}
+            {isMoreLoaded && contentMore}
+          </Text>
+
+          {!isMoreLoaded && (
+            <Button
+              as={Button}
+              width="full"
+              color="blue.700"
+              onClick={() => setIsMoreLoaded(!isMoreLoaded)}
+            >
+              더보기
+            </Button>
+          )}
+          <Text fontSize="12px" color="gray">
+            {getDateFormat(createdAt)}
+          </Text>
+        </Flex>
       </Flex>
-    </Box>
+    </>
   );
 }
