3aaf8bbae1d6c9138524421a91d34aa5bb68b55f
codeisneverodd <codeisneverodd@gmail.com>
2023-03-02 15:50:53 +0900

    feat: add feedsKey main

--- src/queryKeys/feedsKeys.ts
+++ src/queryKeys/feedsKeys.ts
@@ -4,5 +4,6 @@ export default {
   replies: (feedId: number) => ["feeds", "replies", feedId] as const,
   rereplies: (feedId: number, replyId: number) =>
     ["feeds", "rereplies", feedId, replyId] as const,
-  likeUsers: (feedId: number) => ["feeds", "likes", feedId] as const
+  likeUsers: (feedId: number) => ["feeds", "likes", feedId] as const,
+  main: () => ["feeds", "main"] as const
 };
