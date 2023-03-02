55fa4ffe1e0e66fc465c90a4c24edbe517e9b4f7
codeisneverodd <codeisneverodd@gmail.com>
2023-03-02 17:03:37 +0900

    feat: add resetPassword API

--- src/api/users/password.ts
+++ src/api/users/password.ts
@@ -1,7 +1,26 @@
 import api from "..";
 
-export const patchPassword = (beforePassword: string, afterPassword: string) =>
+export const patchPassword = ({
+  beforePassword,
+  afterPassword
+}: {
+  beforePassword: string;
+  afterPassword: string;
+}) =>
   api.prod.patch("/api/users/password", {
     beforePassword,
+    afterPassword
+  });
+
+// TODO: 비밀번호 재설정 API 연결
+export const resetPassword = ({
+  authKey,
+  afterPassword
+}: {
+  authKey: string;
+  afterPassword: string;
+}) =>
+  api.prod.patch("/some-api-here", {
+    authKey,
     afterPassword
   });
