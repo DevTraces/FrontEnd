fd14cd4cd91b34cb7fa174d084d516e66654fadb
codeisneverodd <codeisneverodd@gmail.com>
2023-03-03 00:16:28 +0900

    feat: add NewFeedList Component

---
+++ src/components/@common/NewFeedList/index.tsx
@@ -0,0 +1,31 @@
+import feedAtom from "@/atoms/feedAtom";
+import { FeedData } from "@/types/data/feed";
+import { Flex } from "@chakra-ui/react";
+import { RecoilRoot } from "recoil";
+import NewFeedCard from "../NewFeedCard";
+
+type FeedListProps = {
+  feedsData: FeedData[];
+};
+
+export default function NewFeedList({ feedsData }: FeedListProps) {
+  return (
+    <Flex
+      direction="column"
+      w="full"
+      mt={{ sm: "80px", md: "0" }}
+      h={{ sm: "calc(100vh - 140px)", md: "100vh" }}
+    >
+      {feedsData.map(feed => (
+        <RecoilRoot
+          key={feed.feedId}
+          initializeState={snapshot => {
+            snapshot.set(feedAtom, feed);
+          }}
+        >
+          <NewFeedCard feedData={feed} />
+        </RecoilRoot>
+      ))}
+    </Flex>
+  );
+}
