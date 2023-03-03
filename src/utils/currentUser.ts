c9da153a3460b58a09cff8a3b64f94c864090a78
jiseung-kang <ill0o0lli@ajou.ac.kr>
2023-03-03 23:02:32 +0900

    feat: add common redirection serverprops

--- src/utils/currentUser.ts
+++ src/utils/currentUser.ts
@@ -4,12 +4,22 @@ import {
   getCookie,
   setCookie
 } from "cookies-next";
+import { IncomingMessage, ServerResponse } from "http";
 
 const isStringCookie = (cookie: CookieValueTypes): cookie is string =>
   typeof cookie === "string";
 
-const getNickname = () => {
-  const nickname = getCookie("nickname");
+type CookieOptions = {
+  res?: ServerResponse<IncomingMessage>;
+  req?: IncomingMessage & {
+    cookies: Partial<{
+      [key: string]: string;
+    }>;
+  };
+};
+
+const getNickname = ({ res, req }: CookieOptions = {}) => {
+  const nickname = getCookie("nickname", { res, req });
 
   return isStringCookie(nickname) ? nickname : "nouser";
 };
@@ -18,10 +28,14 @@ const setNickname = (nickname: string) => setCookie("nickname", nickname);
 
 const removeNickname = () => deleteCookie("nickname");
 
+const isValidUser = ({ res, req }: CookieOptions = {}) =>
+  getNickname({ res, req }) !== "nouser" && getNickname({ res, req }) !== "";
+
 const currentUser = {
   getNickname,
   setNickname,
-  removeNickname
+  removeNickname,
+  isValidUser
 };
 
 export default currentUser;
