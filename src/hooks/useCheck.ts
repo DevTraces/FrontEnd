48e47ea3eb3d1cbbd2aa2c2cd56c411bc7ca92ea
codeisneverodd <codeisneverodd@gmail.com>
2023-03-03 20:01:09 +0900

    feat: add emailAuthKeyCheckForResetMutation

--- src/hooks/useCheck.ts
+++ src/hooks/useCheck.ts
@@ -1,6 +1,7 @@
 import { postEmailAuthKeyCheck } from "@/api/auth/email/auth-key/check";
 import { getEmailDuplicateCheck } from "@/api/users/email/check";
 import { getNicknameDuplicateCheck } from "@/api/users/nickname/check";
+import { postPasswordEmailCheck } from "@/api/users/password/email/check";
 import { useToast } from "@chakra-ui/react";
 import { useMutation } from "@tanstack/react-query";
 
@@ -50,10 +51,30 @@ export default function useCheck() {
       });
     }
   });
+  const emailAuthKeyCheckForResetMutation = useMutation({
+    mutationFn: postPasswordEmailCheck,
+    onSuccess: ({ isCorrect }) => {
+      if (!isCorrect) {
+        toast({
+          title: "인증코드가 올바르지 않아요.",
+          status: "warning",
+          duration: 3000
+        });
+      }
+    },
+    onError: () => {
+      toast({
+        title: "인증코드가 확인 중 문제가 발생했어요.",
+        status: "error",
+        duration: 3000
+      });
+    }
+  });
 
   return {
     nicknameDuplicateMutation,
     emailDuplicateMutation,
-    emailAuthKeyCheckMutation
+    emailAuthKeyCheckMutation,
+    emailAuthKeyCheckForResetMutation
   };
 }
