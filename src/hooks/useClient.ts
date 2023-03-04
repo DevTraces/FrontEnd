77a2da9bcda3ca35f09b4cb92a7c133bfb8f810c
jiseung-kang <ill0o0lli@ajou.ac.kr>
2023-03-04 18:33:58 +0900

    feat: add useClient

---
+++ src/hooks/useClient.ts
@@ -0,0 +1,13 @@
+import { useState, useEffect } from "react";
+
+const useClient = () => {
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    if (typeof window !== "undefined") setIsClient(true);
+  }, []);
+
+  return isClient;
+};
+
+export default useClient;
