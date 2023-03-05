9814d327c09ca65343fec0022840806d5d0f877a
jiseung-kang <ill0o0lli@ajou.ac.kr>
2023-03-05 16:55:27 +0900

    feat: add tags validation

--- src/components/post/FeedEditor/Tags/index.tsx
+++ src/components/post/FeedEditor/Tags/index.tsx
@@ -5,7 +5,8 @@ import {
   Input,
   InputGroup,
   InputLeftAddon,
-  useToast
+  useToast,
+  Text
 } from "@chakra-ui/react";
 import { faHashtag, faX } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -15,70 +16,89 @@ type TagItem = { id: string; content: string };
 
 type TagsProps = {
   tags: TagItem[];
-  onAddTag: (content: TagItem["content"]) => void;
+  onAddTag: (content: TagItem["content"]) => Promise<void>;
   onRemoveTag: (id: TagItem["id"]) => void;
 };
 
 export default function Tags({ tags, onAddTag, onRemoveTag }: TagsProps) {
   const [value, setValue] = useState("");
   const toast = useToast();
+  const [errorMsg, setErrorMsg] = useState("");
 
   const addTag = (content: string) => {
-    if (content.length > 100) {
-      toast({
-        title: "태그는 100자 이하만 가능해요",
-        status: "warning",
-        duration: 1000
-      });
-    } else if (tags.length >= 10) {
+    if (tags.length >= 10) {
       toast({
         title: "태그는 최대 10개만 가능해요",
         status: "warning",
         duration: 1000
       });
     } else {
-      onAddTag(content);
+      onAddTag(content)
+        .then(() => {
+          setErrorMsg("");
+        })
+        .catch((err: Error) => {
+          setErrorMsg(err.message);
+        });
       setValue("");
     }
   };
 
   return (
-    <Flex bg="white" w="full" px="32px" py="16px" gap="12px" flexWrap="wrap">
-      {tags.map(t => (
-        <Button
-          key={t.id}
-          onClick={() => onRemoveTag(t.id)}
-          rightIcon={
-            <Icon as={FontAwesomeIcon} icon={faX} color="gray.400" size="xs" />
-          }
-        >
-          {t.content}
-        </Button>
-      ))}
-      <form
-        onSubmit={e => {
-          e.preventDefault();
-          addTag(value);
-        }}
+    <>
+      <Text
+        color="orange.400"
+        bg="white"
+        textAlign="left"
+        w="full"
+        h="40px"
+        px="32px"
+        py="16px"
       >
-        <InputGroup
-          flex={{ sm: "none", md: "1" }}
-          border="transparent"
-          w={{ sm: "full", md: "inherit" }}
+        {errorMsg}
+      </Text>
+      <Flex bg="white" w="full" px="32px" py="16px" gap="12px" flexWrap="wrap">
+        {tags.map(t => (
+          <Button
+            key={t.id}
+            onClick={() => onRemoveTag(t.id)}
+            rightIcon={
+              <Icon
+                as={FontAwesomeIcon}
+                icon={faX}
+                color="gray.400"
+                size="xs"
+              />
+            }
+          >
+            {t.content}
+          </Button>
+        ))}
+        <form
+          onSubmit={e => {
+            e.preventDefault();
+            addTag(value);
+          }}
         >
-          <InputLeftAddon bg="white" pr="0" pl={{ sm: "0", md: "inherit" }}>
-            <Icon as={FontAwesomeIcon} icon={faHashtag} color="gray.400" />
-          </InputLeftAddon>
-          <Input
-            placeholder="태그를 입력 후 엔터"
-            border="none"
-            focusBorderColor="transparent"
-            outline="none"
-            value={value}
-            onChange={e => setValue(e.target.value)}
-          />
-        </InputGroup>
-      </form>
-    </Flex>
+          <InputGroup
+            flex={{ base: "none", md: "1" }}
+            border="transparent"
+            w={{ base: "full", md: "inherit" }}
+          >
+            <InputLeftAddon bg="white" pr="0" pl={{ base: "0", md: "inherit" }}>
+              <Icon as={FontAwesomeIcon} icon={faHashtag} color="gray.400" />
+            </InputLeftAddon>
+            <Input
+              placeholder="태그를 입력 후 엔터"
+              border="none"
+              focusBorderColor="transparent"
+              outline="none"
+              value={value}
+              onChange={e => setValue(e.target.value)}
+            />
+          </InputGroup>
+        </form>
+      </Flex>
+    </>
   );
 }
