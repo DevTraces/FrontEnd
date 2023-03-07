708e1721270840bcf26bab9610080509ed9e234a
jiseung-kang <ill0o0lli@ajou.ac.kr>
2023-03-07 13:20:09 +0900

    fix: date format

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -6,15 +6,15 @@ const getDateFormat = (date: Date | string) => {
 
   const diff = now.getTime() - target.getTime();
 
-  if (diff < 1000 * 60) return "now";
+  if (diff < 1000 * 60) return "방금";
 
-  if (diff < 1000 * 60 * 60) return `${Math.floor(diff / (1000 * 60))}m`;
+  if (diff < 1000 * 60 * 60) return `${Math.floor(diff / (1000 * 60))}분`;
 
-  if (diff < DAY_TIME) return `${Math.floor(diff / (1000 * 60 * 60))}h`;
+  if (diff < DAY_TIME) return `${Math.floor(diff / (1000 * 60 * 60))}시간`;
 
-  if (diff < DAY_TIME * 7) return `${Math.floor(diff / DAY_TIME)}d`;
+  if (diff < DAY_TIME * 7) return `${Math.floor(diff / DAY_TIME)}일`;
 
-  return `${Math.floor(diff / (DAY_TIME * 7))}w`;
+  return `${Math.floor(diff / (DAY_TIME * 7))}주`;
 };
 
 export default getDateFormat;
