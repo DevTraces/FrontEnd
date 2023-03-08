82e1c3adf9dfa52eccc06b740e4a0f012afd03b7
codeisneverodd <codeisneverodd@gmail.com>
2023-03-08 12:51:13 +0900

    feat: change 404 page style

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,28 +1,117 @@
 import NavLayout from "@/components/@common/NavLayout";
-import { Button, Center, Flex, Text } from "@chakra-ui/react";
+import useClient from "@/hooks/useClient";
+import {
+  Box,
+  Button,
+  Center,
+  Divider,
+  Flex,
+  Icon,
+  Text
+} from "@chakra-ui/react";
+import { faCat } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { motion } from "framer-motion";
+import Image from "next/image";
 import Link from "next/link";
+import NotFoundPageImg from "public/404.svg";
 
+const shuffle = (arr: string[]) => {
+  const result = arr.slice();
+  for (let i = result.length - 1; i > 0; i -= 1) {
+    const randomPosition = Math.floor(Math.random() * (i + 1));
+    const temp = result[i];
+    result[i] = result[randomPosition];
+    result[randomPosition] = temp;
+  }
+  return result;
+};
 export default function NotFoundPage() {
+  const isClient = useClient();
+
+  const developers = shuffle([
+    "codeisneverodd",
+    "jiseung-kang",
+    "DongvinPark",
+    "gwakjaeha",
+    "heyazoo1007"
+  ]);
+
+  if (!isClient) return null;
   return (
     <NavLayout>
-      <Center>
+      <Center
+        pos="relative"
+        alignItems="center"
+        w="full"
+        flexDirection="column"
+        pt="200px"
+      >
         <Flex
           direction="column"
           pt={20}
-          gap={5}
+          gap="40px"
           m="auto"
           mb={20}
           textAlign="center"
         >
-          <Text fontWeight="bold">
-            죄송합니다. 페이지를 사용할 수 없습니다.
+          <motion.div
+            animate={{
+              scale: [1, 2, 2, 1, 1],
+              rotate: [0, 0, 180, 180, 0],
+              borderRadius: ["0%", "0%", "50%", "50%", "0%"]
+            }}
+            transition={{
+              duration: 2,
+              ease: "easeInOut",
+              times: [0, 0.2, 0.5, 0.8, 1],
+              repeat: Infinity,
+              repeatDelay: 1
+            }}
+          >
+            <Image src={NotFoundPageImg} alt="이미지" width={300} />
+          </motion.div>
+          <Text fontWeight="bold" fontSize="xl">
+            사용할 수 없는 페이지에요.
           </Text>
-          <Text>클릭하신 링크가 잘못되었거나 페이지가 삭제되었습니다.</Text>
           <Link href="/feed">
-            <Button color="primary" bg="gray.300" fontWeight="bold">
-              ArtBubble로 돌아가기
+            <Button fontWeight="bold" size="lg" colorScheme="purple">
+              메인으로 돌아가기
             </Button>
           </Link>
+          <Divider />
+          <Flex
+            alignItems="center"
+            textAlign="center"
+            justifyContent="center"
+            direction="column"
+          >
+            <Box>
+              <Text fontSize="2xl">아래 개발자 중 한 명을 잡아보세요!</Text>
+            </Box>
+            <motion.div
+              initial={{ x: -500 }}
+              animate={{ x: 500 }}
+              transition={{
+                repeat: Infinity,
+                repeatType: "reverse",
+                duration: 1.3
+              }}
+            >
+              <Flex gap="40px" mt="40px">
+                {developers.map(d => (
+                  <Link href={`https://github.com/${d}`} target="_blank">
+                    <Button
+                      colorScheme="facebook"
+                      leftIcon={<Icon as={FontAwesomeIcon} icon={faCat} />}
+                    >
+                      {d}
+                    </Button>
+                  </Link>
+                ))}
+              </Flex>
+            </motion.div>
+          </Flex>
         </Flex>
       </Center>
     </NavLayout>
